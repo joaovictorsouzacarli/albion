0bd2e6e4def58cad1688e8d36cc178b5c1f54433
João Victor <joao.carli@unesp.br>
2025-05-19 02:00:03 -0300

    026 Ajuste

--- components/ranking-tabs.tsx
+++ components/ranking-tabs.tsx
@@ -3,16 +3,24 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { DpsRanking } from "@/components/dps-ranking"
 import { HpsRanking } from "@/components/hps-ranking"
+import { ParticipacaoRanking } from "@/components/participacao-ranking"
+import { Flame, Heart, Users } from "lucide-react"
 
 export function RankingTabs() {
   return (
     <Tabs defaultValue="dps" className="w-full">
-      <TabsList className="grid w-full grid-cols-2 bg-black border border-blue-900/50">
+      <TabsList className="grid w-full grid-cols-3 bg-black border border-blue-900/50">
         <TabsTrigger value="dps" className="data-[state=active]:bg-[#00c8ff] data-[state=active]:text-black">
+          <Flame className="h-4 w-4 mr-2" />
           TOP DPS
         </TabsTrigger>
         <TabsTrigger value="hps" className="data-[state=active]:bg-[#00c8ff] data-[state=active]:text-black">
+          <Heart className="h-4 w-4 mr-2" />
           TOP HPS
+        </TabsTrigger>
+        <TabsTrigger value="participacao" className="data-[state=active]:bg-[#00c8ff] data-[state=active]:text-black">
+          <Users className="h-4 w-4 mr-2" />
+          PARTICIPAÇÃO
         </TabsTrigger>
       </TabsList>
       <TabsContent value="dps">
@@ -21,7 +29,9 @@ export function RankingTabs() {
       <TabsContent value="hps">
         <HpsRanking />
       </TabsContent>
+      <TabsContent value="participacao">
+        <ParticipacaoRanking />
+      </TabsContent>
     </Tabs>
   )
 }
-
